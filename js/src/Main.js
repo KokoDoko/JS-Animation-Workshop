eea82fb01a2f0acdfffe57094ef98c8ae9954b49
Kokodoko <benni_jolink@hotmail.com>
2015-10-02 14:27:54 +0200

    Removed local library files

--- js/src/Main.js
+++ js/src/Main.js
@@ -82,29 +82,41 @@
 		// check the width of the race track and use that as a finishing point for our animation
 		var finish = $("#container").width();
 
-		
+		// we use a loop to define four random speeds, and also to get a reference to each individual kart.
 		for(var i = 0; i<4; i++){
+			// random speed for a kart in seconds
 			var speed = 1 + Math.random() * 3;
+			// get the individual kart reference. this will be used to tween this kart, but also to tell the callback function which kart has finished
 			var kart = $("#container").children().eq(i);
-			TweenLite.to(kart,speed,{x:finish,ease:Cubic.easeInOut,onComplete: evaluateKart, onCompleteParams:[kart, i]});
+			// now we create the actual tween animation
+			// we tell tweenlite to animate the kart at a speed. 
+			// we use easing to simulate a accelerate and brake effect for the animation
+			// we also add a CALLBACK function, this will be called when the animation has finished (the kart has passed the finish)
+			// and lastly, we define the parameter for the callback function, which is a reference to the kart that has finished
+			TweenLite.to(kart,speed,{x:finish,ease:Cubic.easeInOut,onComplete: evaluateKart, onCompleteParams:[kart]});
 		}
 	}
 
 	// ************************************************************
 	//
-	// kart gaat over de finishlijn
+	// A KART PASSES THE FINISH LINE
+	// THIS IS CALLED WHEN THE RACING ANIMATION FOR ONE KART HAS COMPLETED
 	//
 	// ************************************************************
-	function evaluateKart(kart, i) {
-		// de eerste keer is de winnaar
+	function evaluateKart(kart) {
+		// update the number of karts that has finished
 		kartsFinished++;
+		// we check how many karts have finished
 		if(kartsFinished == 1) {
-			// toon powerup
+			// use jQuery to add a mushroom image to the DOM
 			var powerup = $("<div class='mushroom'></div>");
 			kart.append(powerup);
+			// now we animate the mushroom image 5 times
+			// note that we also use this function to wait for all the karts to have finished
+			// after that we use the callback function APPEARATSTART to restart the whole process
 			TweenMax.to(powerup, 0.7, {y:"-=50", alpha:0, repeat:5, onComplete:appearAtStart});
 		} else {
-			// teleurgesteld wegrijden
+			// this kart has not finished first, so it just drives away
 			TweenLite.to(kart, 0.7, {x:"+=200", ease:Cubic.easeIn, delay:0.3});
 		}
 	}
